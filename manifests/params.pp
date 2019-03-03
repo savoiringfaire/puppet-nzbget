634f24f5254b9eaa30a65f780aa3233c2c1ce9e9
declanthebritton <47713761+declanthebritton@users.noreply.github.com>
2019-03-03 15:20:29 +0000

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,10 +12,6 @@ class nzbget::params {
 
   $masks = [$::nzbget::manage_service_dirs[1], $::nzbget::manage_data_dirs[1]]
 
-  if (!is_absolute_path($::nzbget::service_dir)) {
-    fail "nzbget::service_dir MUST be an absolute path [${::nzbget::service_dir}]."
-  }
-
   $daemon_required_mounts = delete(unique([
     $::nzbget::service_dir,
     $::nzbget::main_dir,
